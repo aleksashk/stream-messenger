241a38dbc8d296b54ee954f53d37f3255e006330
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-08-22 11:30:12 +0300

    initial commit

---
+++ src/main/java/com/flameksandr/streammessenger/StreamMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.flameksandr.streammessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flameksandr/streammessenger/StreamMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flameksandr.streammessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
